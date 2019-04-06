6a536d48f9cba217c5502984c3e30c4006de005a
Aaron Tekippe <atekippe@hotmail.com>
2019-04-06 09:20:36 -0500

    Updated script to update index.html with MQTT topic

--- mqtt.py
+++ mqtt.py
@@ -35,6 +35,10 @@ def on_message(client, userdata, msg):
         print("YES")
         os.system("killall feh")
         write_image(msg.payload.decode())
+    if str(msg.topic) in "the_gibson/haxor":
+        f = open("index.html", "a")
+        f.write(msg.payload.decode() + "\r\n")
+        f.close()
 
 def on_connect(client, userdata,flags, rc):
     print("Connected to Broker")
@@ -56,6 +60,6 @@ client.on_publish = on_publish
 client.username_pw_set(user, password)
 
 client.connect(broker,port)
-client.subscribe(topic="the_gibson/test", qos=0)
+client.subscribe(topic="the_gibson/#", qos=0)
 
 client.loop_forever()
