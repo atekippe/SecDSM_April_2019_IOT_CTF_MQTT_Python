794d68b53e85cdf0529083f1196e3064413b9fc4
Aaron Tekippe <atekippe@hotmail.com>
2019-04-07 08:28:13 -0500

    added mqtt client script

--- webpage_mqtt_client.py
+++ webpage_mqtt_client.py
@@ -1,22 +1,5 @@
 import paho.mqtt.client as mqtt
-import time
-import os
-from PIL import Image, ImageDraw
-from PIL import ImageFont
 
-
-
-def write_image(payload):
-    img = Image.new('RGB', (480, 272), color=(0, 0, 0))
-
-    d = ImageDraw.Draw(img)
-
-    font = ImageFont.truetype("Lato-Black.ttf", 32)
-    d.text((150, 100), payload, fill=(32, 194, 14), font=font)
-
-    img.save('pil_text.png')
-    #img.show()
-    os.system('feh pil_text.png &')
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed to topic")
@@ -30,16 +13,12 @@ def on_publish(client, userdata, mid):
 
 def on_message(client, userdata, msg):
     print(msg.topic, msg.payload.decode())
-    print(msg.topic)
 
-    if str(msg.topic) in "the_gibson/test":
-        print("YES")
-        os.system("killall feh")
-        write_image(msg.payload.decode())
     if str(msg.topic) in "the_gibson/haxor":
         f = open("index.html", "a")
         f.write(msg.payload.decode() + "\r\n")
         f.close()
+
 
 def on_connect(client, userdata,flags, rc):
     print("Connected to Broker")
